9227c8ef220fa40c04ae5ebf80ca476865d1cc7c
Ttibsi <ashisbitt@icloud.com>
2022-12-29 17:06:17 +0000

    generics

---
+++ rust-book/generics/src/main.rs
@@ -0,0 +1,36 @@
+// Function using generics
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for i in list {
+        if i > largest { largest = i; }
+    }
+
+    return largest;
+}
+
+// Struct using generics
+#[derive(Debug)]
+struct Point<T> {
+    x:T,
+    y:T,
+}
+
+// generic struct methods
+impl<T> Point<T>{
+    fn x(&self) -> &T {return &self.x;}
+}
+
+fn main() {
+    let num_list = vec![4,18, 12, 69, 420, 34];
+    let char_list = vec!['a', 'g', 'y', 'w', 'p', 'c', 't'];
+
+    println!("Largest number is: {}", largest(&num_list));
+    println!("Largest char is: {}", largest(&char_list));
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+    println!("{:?}", integer);
+    println!("{:?}", float);
+    println!("int x = {}", integer.x());
+}
